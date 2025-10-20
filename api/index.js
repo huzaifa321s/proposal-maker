e47c93cbe7e484286fd203a07a303729c9a6f4aa
huzaifa321s <muhammadhuzaifa@humantekteam.com>
2025-10-20 16:18:46 +0500

    index.js

---
+++ api/index.js
@@ -0,0 +1,28 @@
+// api/index.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "../src/config/db.js";
+import profileRoutes from '../src/routes/profile.js';
+import proposalRoutes from '../src/routes/proposal.js';
+import ServerlessHttp from "serverless-http";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ limit: "60mb", extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running successfully (In-House Proposal System API)");
+});
+app.use('/api', profileRoutes);
+app.use('/api/proposals', proposalRoutes);
+
+// Export handler for Vercel
+export const handler = ServerlessHttp(app);
