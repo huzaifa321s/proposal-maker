06d8fa398b0c9b231b016634c791db1144144537
huzaifa321s <muhammadhuzaifa@humantekteam.com>
2026-05-17 23:12:39 -0700

    fix: allow all origins in socket.io cors config

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/stream', (req, res) => {
 // === Socket.io Setup ===
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5000', 'http://localhost:5173', 'http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST'],
   },
   maxHttpBufferSize: 1e8,
